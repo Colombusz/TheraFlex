539c9e02e9b2699189aa019c139e19010f7f4b12
ELI <Eliseo.lim12@gmail.com>
2024-02-28 00:04:49 +0800

    FUCKING MANAGES

--- Thera/database/migrations/2024_02_27_160313_add_managers_id_and_employees_id_to_managers_assignments_table.php
+++ Thera/database/migrations/2024_02_27_160313_add_managers_id_and_employees_id_to_managers_assignments_table.php
@@ -12,10 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('managers_assignments', function (Blueprint $table) {
-            $table->unsignedBigInteger('employees_id')->index();
-            $table->unsignedBigInteger('managers_id')->index();
-            $table->foreign('employees_id')->references('id')->on('employees')->onDelete('cascade');
-            $table->foreign('managers_id')->references('id')->on('managers')->onDelete('cascade');
+            //
         });
     }
 
@@ -25,8 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('managers_assignments', function (Blueprint $table) {
-            $table->dropColumn('employees_id');
-            $table->dropColumn('managers_id');
+            //
         });
     }
 };
